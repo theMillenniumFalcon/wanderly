a6a2b428eb0c8ffc94262587c6bf09bcfebdd59a
themillenniumfalcon <nishankpr2002@gmail.com>
2024-10-21 01:31:40 +0530

    added from label in form

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,26 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+import { formSchema } from "@/lib/schema/formSchema";
+
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/seperator";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 export const runtime = "edge";
 
 export default function Home() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fromLocation: ""
+    },
+  })
+
+  const onSubmit = () => {}
+
   return (
     <div className="w-full mt-8">
       <h1 className="md:text-4xl sm:text-3xl text-2xl mb-4 font-semibold">
@@ -23,6 +40,26 @@ export default function Home() {
       </div>
       <Separator className="my-8" />
       
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-4 mt-4 mb-24"
+        >
+          <FormField
+            control={form.control}
+            name="fromLocation"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Where are you travelling from?</FormLabel>
+                <FormControl>
+                  <Input placeholder="e.g. Toronto, ON" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
     </div>
   );
 }
